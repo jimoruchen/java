f454ae35926d00e942bc3b0a9ca54c436af246c6
jimoruchen <1007896918@qq.com>
2024-11-24 21:38:41 +0800

    11.24

--- leetcode01/Solution1.java
+++ leetcode01/Solution1.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Solution1 {
-    public int[] twoSum(int[] nums, int target) {
+    public static int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             if(map.containsKey(target - nums[i])) {
