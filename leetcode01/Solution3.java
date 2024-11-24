16b17ce00af7676b15dbf3a76229b99541f1dfc7
jimoruchen <1007896918@qq.com>
2024-11-24 21:22:16 +0800

    11.24

--- leetcode01/Solution3.java
+++ leetcode01/Solution3.java
@@ -1,4 +1,10 @@
 package leetcode01;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution3 {
+    public static void main(String[] args) {
+        Map<Integer, Integer> map = new HashMap<>();
+    }
 }
