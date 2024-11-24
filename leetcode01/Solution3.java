e8b11016965a5ce29e347aa11b9f60e60f87b2cb
jimoruchen <1007896918@qq.com>
2024-11-24 21:28:01 +0800

    11.24

--- leetcode01/Solution3.java
+++ leetcode01/Solution3.java
@@ -4,7 +4,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Solution3 {
-    public static void main(String[] args) {
+    public static int[] toSum(int[] nums) {
         Map<Integer, Integer> map = new HashMap<>();
+        return new int[0];
     }
 }
